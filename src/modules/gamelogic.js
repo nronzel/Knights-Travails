1580c2991974ac2ee064ec7c035c0183c063e5dc
nronzel <86695181+nronzel@users.noreply.github.com>
2023-02-13 11:43:32 -0500

    added gamelogic.js
    This file will contain the main logic behind getting
    start/end coordinates and determining the path.

---
+++ src/modules/gamelogic.js
@@ -0,0 +1,13 @@
+// choose start position
+// choose end position
+// show shortest path to end position
+//   const validMoves = [
+//     [1, 2],
+//     [2, 1],
+//     [-1, 2],
+//     [-2, 1],
+//     [-2, -1],
+//     [-1, -2],
+//     [1, -2],
+//     [2, -1],
+//   ];
