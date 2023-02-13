4142b396b6800450aa1414c0d3bc2d7ddac27afd
nronzel <86695181+nronzel@users.noreply.github.com>
2023-02-13 11:43:14 -0500

    cleaned up comments

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -106,17 +106,3 @@ export default class UI {
     }
   }
 }
-
-// choose start position
-// choose end position
-// show shortest path to end position
-//   const validMoves = [
-//     [1, 2],
-//     [2, 1],
-//     [-1, 2],
-//     [-2, 1],
-//     [-2, -1],
-//     [-1, -2],
-//     [1, -2],
-//     [2, -1],
-//   ];
